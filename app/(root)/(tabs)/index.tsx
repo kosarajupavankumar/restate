90e677242f8198d2bd62d32fb4cd072d7a4d0513
pavankumar <pavankumarkosaraju@gmail.com>
2024-12-31 16:43:06 +0530

    SignIn page completed

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-        <Text className="font-bold text-lg my-10">Welcome to Restate App</Text>
+        <Text className="font-bold text-lg my-10 font-rubik text-3xl color-amber-700">Welcome to Restate App</Text>
       <Link href="/sign-in">Sign In</Link>
         <Link href="/explore">explore</Link>
         <Link href="/profile">profile</Link>
