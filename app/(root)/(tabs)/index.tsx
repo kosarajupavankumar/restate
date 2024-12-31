e3a2a42d9d0ff7b5347c9b37782d436ab89661cc
pavankumar <pavankumarkosaraju@gmail.com>
2024-12-31 14:44:03 +0530

    NativeWind SetUp

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,21 @@
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+        <Text className="font-bold text-lg my-10">Welcome to Restate App</Text>
+      <Link href="/sign-in">Sign In</Link>
+        <Link href="/explore">explore</Link>
+        <Link href="/profile">profile</Link>
+        <Link href="/properties/1">Property</Link>
+
+    </View>
+  );
+}
