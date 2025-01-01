0580a0d2cddb9cb8132ee6558ddb8032607439fb
pavankumar <pavankumarkosaraju@gmail.com>
2025-01-01 15:38:54 +0530

    Refactor tab layout and restore sign-in functionality

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,12 +10,9 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-        <Text className="font-bold text-lg my-10 font-rubik text-3xl color-amber-700">Welcome to Restate App</Text>
-      <Link href="/sign-in">Sign In</Link>
-        <Link href="/explore">explore</Link>
-        <Link href="/profile">profile</Link>
-        <Link href="/properties/1">Property</Link>
-
+      <Text className="font-bold text-lg my-10 font-rubik text-3xl color-amber-700">
+        Welcome to Restate App
+      </Text>
     </View>
   );
 }
